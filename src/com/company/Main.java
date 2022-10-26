95cd0228f8602dae2985843867ab5d7b9c2e91b2
lida <kurogiri.dabi@gmail.com>
2022-10-26 22:00:40 +0300

    commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,28 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-
+    public static void main(String[] args) throws IOException {
         getWordCount("test.txt");
     }
-    public static void getWordCount(String filename) throws FileNotFoundException {
+
+    public static void getWordCount(String filename) throws IOException {
         File file = new File(filename);
         Scanner scanner = new Scanner(file);
-        int characters = 0;
-        int words = 0;
+
         int gaps = 0;
 
+        String text = "";
         while (scanner.hasNextLine()) {
-            characters++;
-            String[] array = scanner.nextLine().split(" ");
-            words = words + array.length;
+            text = scanner.nextLine();
         }
-            System.out.println("Количество символов = " + characters);
-            System.out.println("Количество символов без пробелов = " + gaps);
-            System.out.println("Количество слов = " + words);
+
+            System.out.println("Количество символов = " + text.length());
+            System.out.println("Количество символов без пробелов = " + text.replaceAll(" ", "").length());
+            System.out.println("Количество слов = " + text.split(" ").length);
+        }
     }
-}
